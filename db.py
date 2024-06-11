d212740b10e14855442dc9a8297afde8aa2eb127
edilson67816051 <serranoluizedil@gmail.com>
2024-06-11 19:21:16 -0400

    powerbi

--- db.py
+++ db.py
@@ -4,5 +4,13 @@ from sqlalchemy.orm import sessionmaker
 
 
 Base = declarative_base()
-engine = create_engine("postgresql://postgres:postgres123@localhost:5432/python_sw")
+engine = create_engine("postgresql://postgres:12345678@localhost:5432/bussines_medical")
 Session = sessionmaker(bind=engine)
+
+# Dependencia para obtener la sesión de base de datos
+def get_db():
+    db = Session()
+    try:
+        yield db
+    finally:
+        db.close()
